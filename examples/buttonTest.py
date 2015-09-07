85427d87d352f076dc57948e1f98833e25625403
Kelvin K <kelvin86@gmail.com>
2015-09-07 07:54:47 -0700

    completed buttonTest and created external pyton config file to store thresholds

--- examples/buttonTest.py
+++ examples/buttonTest.py
@@ -2,6 +2,7 @@
 
 import time
 import RPi.GPIO as GPIO
+import tmsconfig
 
 class ThresholdControl:
 	def __init__(self):
@@ -9,20 +10,55 @@ class ThresholdControl:
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(17,GPIO.IN)
 		
+		#Setup configs
+		self.setThresholdArray()
 		print "Start Loop"
 		self.listener()
 
+	def getThldIdx(self):
+		return self.currThresholdIndex
+
+	def setThresholdArray(self):
+		self.thresholdMap = tmsconfig.temp_thresholds
+		self.currThresholdIndex = 0
+		self.thresholdCount = len(self.thresholdMap)
+		self.setThresholdValues()
+	
+	def setThresholdValues(self):
+		self.th_name = self.thresholdMap[self.getThldIdx()]['name']
+		self.min_temp = self.thresholdMap[self.getThldIdx()]['min_temp']
+		self.max_temp = self.thresholdMap[self.getThldIdx()]['max_temp']
+		#Test print
+		print "Current threshold value: {0} : {1} - {2} ".format(self.th_name,self.min_temp,self.max_temp)
+
 	def listener(self):
 		prev_input = 0
+		prev_input = GPIO.input(17)
 		while True:
 			
 			input= GPIO.input(17)
-			print("input:" +str(input))
+			#print("input:" +str(input))
 			if (not prev_input) and input:
-				print("Button presesd")
+				#print("Button presesd")
+				self.thresholdCycle()
 			#Update prev input
 			prev_input = input
 			time.sleep(0.05)
+
+	def thresholdCycle(self):
+		self.incThldIdx()
+		self.setThresholdValues()
+	
+	def incThldIdx(self):
+		if (self.getThldIdx() >= self.thresholdCount - 1):
+			#Set counter index back to 0
+			self.currThresholdIndex = 0
+		else:
+			self.currThresholdIndex += 1
+		
+	def getThldValues(self):
+		return [self.th_name,self.min_temp,self.max_temp]	
+		
 
 if __name__ == '__main__':
 	instance = ThresholdControl()
---
+++ examples/tmsconfig.py
@@ -0,0 +1,14 @@
+temp_thresholds=[
+	{'name':'A',
+		'min_temp':200,
+		'max_temp':220},
+	{'name':'B',
+		'min_temp':221,
+		'max_temp':240},
+	{'name':'C',
+		'min_temp':241,
+		'max_temp':260},
+	{'name':'D',
+		'min_temp':261,
+		'max_temp':280}
+]
